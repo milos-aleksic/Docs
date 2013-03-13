dba0ea0fb3a490930bbc18a31d9d426ab4207edb
Daniele Rosario <realskullbock@gmail.com>
2013-03-13 18:27:09 +0100

    removed timestamp

--- assets/js/docs.js
+++ assets/js/docs.js
@@ -63,9 +63,7 @@ $(document).ready(function(){
 		if (target.attr('href').substring(0, 4) != 'http' && target.attr('href').substring(0, 1) != '#') {
 			event.stopPropagation();
 			populateWindow(target.attr('href'));
-			var timestamp = new Date();
-			timestamp = timestamp.getTime();
-			history.pushState(state, target.attr('href'), "?" + target.attr('href') + '&v=' + timestamp);
+			history.pushState(state, target.attr('href'), "?" + target.attr('href'));
 			return false;
 		}
 	});
